8b58c4dcf483c4465c3b95b67257399f91b9e377
ASUS-PC <919041098@qq.com>
2021-04-13 16:30:36 +0800

    完成aside

---
+++ src/components/Avatar.vue
@@ -0,0 +1,35 @@
+<template>
+  <span :title="user.username">{{information}}</span>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      user:{
+        username:'baizhe'
+      },
+      information:'L'
+    }
+  }
+};
+</script>
+<style scoped>
+  span{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 32px;
+    border-radius: 50%;
+    background: #f2b81c;
+    color:#fff;
+    text-shadow: 1px 0 1px #795c19;
+    font-weight: bold;
+    text-transform: uppercase;
+    /*字体文本大写*/
+    font-size:18px;
+    margin-top: 15px;
+  }
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import NotebookList from '@/components/NotebookList'
+import NoteDetail from '@/components/NoteDetail'
+import Trash from '@/components/Trash'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path:'/',
+      redirect:'/Login'
+    },
+    {
+      path: '/Login',
+      component: Login
+    }, {
+      path: '/notebooks',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: Trash
+    }
+  ]
+})
