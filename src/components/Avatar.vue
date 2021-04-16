209263dd03c4ad9d64e5de4352bbe6aad19926d7
ASUS-PC <919041098@qq.com>
2021-04-16 10:30:52 +0800

    Avatar组件展示用户信息

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,35 +1,47 @@
 <template>
-  <span :title="user.username">{{information}}</span>
+  <span :title="username">{{ slug }}</span>
 </template>
 
 <script>
+import Auth from '@/apis/auth'
 export default {
-  data(){
-    return{
-      user:{
-        username:'baizhe'
-      },
-      information:'L'
+  data() {
+    return {
+      username: '未登录',
+    }
+  },
+  created() {
+    Auth.getInfo().then(
+      res=>{
+        if(res.isLogin){
+          this.username = res.data.username
+        }
+      }
+    )
+  },
+  computed:{
+    slug(){
+      return this.username.charAt(0)
     }
   }
 };
 </script>
 <style scoped>
-  span{
-    display: inline-block;
-    width: 30px;
-    height: 30px;
-    text-align: center;
-    line-height: 32px;
-    border-radius: 50%;
-    background: #f2b81c;
-    color:#fff;
-    text-shadow: 1px 0 1px #795c19;
-    font-weight: bold;
-    text-transform: uppercase;
-    /*字体文本大写*/
-    font-size:18px;
-    margin-top: 15px;
-  }
+span {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 32px;
+  border-radius: 50%;
+  background: #f2b81c;
+  color: #fff;
+  text-shadow: 1px 0 1px #795c19;
+  font-weight: bold;
+  text-transform: uppercase;
+  /*字体文本大写*/
+  font-size: 18px;
+  margin-top: 15px;
+}
 
 </style>
