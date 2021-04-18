c1ce0f174b7cfef3ae1891690182ec842350c5a9
ASUS-PC <919041098@qq.com>
2021-04-18 22:59:59 +0800

    完成markdown样式引入

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="note" class="detail">
-    <note-sidebar  @update:notes="value=> notes = value"/>
+    <note-sidebar @update:notes="value=> notes = value"/>
     <div class="note-detail">
       <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
-      <div v-show="curNote.id">
+      <div class="note-detail-tc" v-show="curNote.id">
         <div class="note-bar">
           <span> 创建日期:{{ curNote.createdAtFriendly }}</span>
           <span> 更新日期:{{ curNote.updatedAtFriendly }}</span>
           <span>{{ statusText }}</span>
           <span class="iconfont icon-delete" @click="deleteNote"></span>
-          <span class="iconfont icon-fullscreen"></span>
+          <span class="iconfont icon-fullscreen" @click="isShowPreview = !isShowPreview"></span>
         </div>
         <div class="note-title">
           <label>
-            <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入标题">
+            <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入...'"
+                   placeholder="输入标题">
             <!--  keydown 键盘输入时监听          -->
           </label>
         </div>
         <div class="editor">
           <label>
-            <textarea v-show="true" v-model:value="curNote.content" @input="updateNote" @keydown="statusText='正在输入...'" placeholder="输入内容,支持 markdown 的语法"></textarea>
+            <textarea v-show="!isShowPreview" v-model:value="curNote.content" @input="updateNote"
+                      @keydown="statusText='正在输入...'" placeholder="输入内容,支持 markdown 的语法"></textarea>
           </label>
-          <div class="preview markdown-body" v-html="" v-show="false"></div>
+          <div class="preview markdown-body" v-html="previewContent" v-show="isShowPreview"></div>
         </div>
       </div>
     </div>
@@ -34,6 +36,11 @@ import NoteSidebar from "@/components/NoteSidebar"
 import Bus from '@/helpers/bus'
 import _ from 'lodash'
 import Notes from '@/apis/notes'
+import MarkdownIt from 'markdown-it'
+
+let md = new MarkdownIt();
+
+
 export default {
   components: {
     NoteSidebar
@@ -42,7 +49,8 @@ export default {
     return {
       curNote: {},
       notes: [],
-      statusText:'笔记未改动'
+      statusText: '笔记未改动',
+      isShowPreview: false
     }
   },
   created() {
@@ -52,36 +60,42 @@ export default {
           this.$router.push({path: '/login'})
         }
       })
-    Bus.$once('update:notes',value=>{
-      this.curNote = value.find(note=> note.id == this.$route.query.noteId) ||{}
+    Bus.$once('update:notes', value => {
+      this.curNote = value.find(note => note.id == this.$route.query.noteId) || {}
     })
   },
   beforeRouteUpdate(to, from, next) {
     this.curNote = this.notes.find(note => note.id == to.query.noteId) || {}
     next()
   },
-  methods:{
+  computed: {
+    previewContent() {
+      // content为空需要进行处理
+      return md.render(this.curNote.content || '')
+    }
+  },
+  methods: {
     //节流
-    updateNote: _.debounce(function() {
-      Notes.updateNote({ noteId: this.curNote.id },
-        { title: this.curNote.title, content: this.curNote.content })
-        .then(data=>{
+    updateNote: _.debounce(function () {
+      Notes.updateNote({noteId: this.curNote.id},
+        {title: this.curNote.title, content: this.curNote.content})
+        .then(data => {
           this.statusText = '已保存'
-        }).catch(data=>{
+        }).catch(data => {
         this.statusText = '保存出错'
       })
-    },300),
+    }, 300),
     // 300ms内用户输入的内容会合并成一个
-    deleteNote(){
-      Notes.deleteNote({noteId:this.curNote.id})
-      .then(data=>{
-        this.$message.success(data.msg)
-        // 提示放入回收站弹窗
-        this.notes.splice(this.notes.indexOf(this.curNote),1)
-        // 删除之后要把notes从数组中删除
-        this.$router.replace({path:'/note'})
-        // 删除之后路由跳转到note页面 不停留在原页面
-      })
+    deleteNote() {
+      Notes.deleteNote({noteId: this.curNote.id})
+        .then(data => {
+          this.$message.success(data.msg)
+          // 提示放入回收站弹窗
+          this.notes.splice(this.notes.indexOf(this.curNote), 1)
+          // 删除之后要把notes从数组中删除
+          this.$router.replace({path: '/note'})
+          // 删除之后路由跳转到note页面 不停留在原页面
+        })
     }
   }
 }
